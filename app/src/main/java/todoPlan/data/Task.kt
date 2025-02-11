29459aae013148e3d7377bde7fe4c8999b43c492
koiama <groznaaksenia@gmail.com>
2025-02-11 05:37:40 +0300

    categories added

--- app/src/main/java/todoPlan/data/Task.kt
+++ app/src/main/java/todoPlan/data/Task.kt
@@ -1,11 +1,22 @@
 package todoPlan.data
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.time.LocalDate
 import java.time.LocalTime
 
-@Entity(tableName = "tasks")
+@Entity(
+    tableName = "tasks",
+    foreignKeys = [
+        ForeignKey(
+            entity = Category::class,
+            parentColumns = ["id"],
+            childColumns = ["categoryId"],
+            onDelete = ForeignKey.SET_NULL
+        )
+    ]
+)
 data class Task(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
@@ -13,5 +24,6 @@ data class Task(
     val name: String,
     val startTime: LocalTime,
     val durationMinutes: Int,
-    val isCompleted: Boolean = false
+    val isCompleted: Boolean = false,
+    val categoryId: Int? = null  // The task may not have a category
 )
