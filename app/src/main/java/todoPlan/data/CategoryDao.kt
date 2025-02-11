6adfbc5683195258517a7e8873c1fb9bca4c2da0
koiama <groznaaksenia@gmail.com>
2025-02-11 05:37:40 +0300

    categories added

---
+++ app/src/main/java/todoPlan/data/CategoryDao.kt
@@ -0,0 +1,20 @@
+package todoPlan.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CategoryDao {
+    @Insert
+    suspend fun insert(category: Category)
+
+    @Delete
+    suspend fun delete(category: Category)
+
+    @Query("SELECT * FROM categories")
+    fun getAllCategories(): Flow<List<Category>>
+
+}
